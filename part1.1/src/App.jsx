952ff489b834a7f9feb0c73069ad98fd98e14bd3
Grace <gracelearnsforever@gmail.com>
2024-02-26 22:19:10 -0800

    Step 1

--- part1.1/src/App.jsx
+++ part1.1/src/App.jsx
@@ -1,35 +1,49 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
+const Header = (props) => {
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <h1>{props.course}</h1>
     </>
   )
 }
 
+const Content = ({parts}) => {
+  return (
+    parts.map((item, id) => {
+      return <p key={id}>{item.chapter} {item.numOfExercises}
+      </p>
+    })
+  )
+
+}
+
+const Total = ({parts}) => {
+  return (
+    <p>Number of exercises {parts.reduce((sum, obj) => { return sum += obj.numOfExercises}, 0)}</p>
+  )
+}
+
+const App = () => {
+  const course = 'Half Stack application development';
+  const parts = [{
+    chapter: 'Fundamentals of React',
+    numOfExercises: 10
+  },
+  {
+    chapter:'Using props to pass data',
+    numOfExercises: 7
+  },
+  {
+    chapter: 'State of a component',
+    numOfExercises: 14
+  }];
+
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content parts={parts} />
+      <Total parts={parts} />
+    </div>
+  )
+}
 export default App
